63fc6849dc0d03279e8e7f24fdc7cdac61bfa5a8
KZhukow <kosty.zhukov.2011@gmail.com>
2023-07-03 12:20:55 +0300

    feat: add interface for registration request

---
+++ src/components/RegistrationFormContainer/RegistrationForm/types.ts
@@ -0,0 +1,12 @@
+export interface IPostRegistrationBody {
+  firstName: string,
+  lastName: string,
+  nationality: string,
+  email: string,
+  dayOfBirth: string,
+  monthOfBirth: string,
+  yearOfBirth: string,
+  gender: string,
+  password: string,
+  repeatPassword: string,
+}
