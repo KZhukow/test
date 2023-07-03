9fe49dd4edb99617889bae27936091d7ca2ff05e
KZhukow <kosty.zhukov.2011@gmail.com>
2023-07-03 12:57:34 +0300

    feat: add RegisteredPage to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './components/MainLayout/MainLayout';
 import RegistrationFormContainer from './components/RegistrationFormContainer/RegistrationFormContainer';
+import RegisteredPage from './components/RegisteredPage/RegisteredPage';
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<RegistrationFormContainer />} />
         <Route path="signup" element={<RegistrationFormContainer />} />
-        <Route path="registered" element="{<Registered />}" />
+        <Route path="registered" element={<RegisteredPage />} />
         <Route path="*" element="Page not found" />
       </Route>
     </Routes>
